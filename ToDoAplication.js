e3f44e57cb7ac6e501e549f00c015c321853943d
aleksandrajagodzinska <aleksandra.berry01@gmail.com>
2017-12-27 23:45:02 +0100

    new element added

--- ToDoAplication.js
+++ ToDoAplication.js
@@ -7,10 +7,15 @@ toDoList.pop(new Task("kanapki","zrobić kanapki"));
 toDoList.pop(new Task("kanapki2","zrobić kanapki33"));
 toDoList.pop(new Task("kanapki3","zrobić kanapki33"));
 
-var nextRow; //= document.createElement("row");
+var nextRow = null; //= document.createElement("row");
+var table = null;
 
 $("button.add").click(function(){
-    nextRow = document.createElement("row task");
+    nextRow = document.createElement('row task');
+    nextRow.innerHTML = "<h1> dupsko</h1>";
+
+    table = document.getElementsByClassName('content');
+    document.body.insertBefore(nextRow,table);
     alert("dupa!");
 
     return nextRow; //new Task(#exampleInputEmail3,#exampleInputPassword3);
