935d2c6f66700e4fe18bcf2e370902f309be8369
aleksandrajagodzinska <aleksandra.berry01@gmail.com>
2017-12-27 22:45:19 +0100

    Prompt and function added

--- ToDoAplication.js
+++ ToDoAplication.js
@@ -1,1 +1,14 @@
-var task = {}
+function Task (title,description) {
+    this.title = title;
+    this.description = description
+}
+var toDoList = new Array();
+toDoList.pop(new Task("kanapki","zrobić kanapki"));
+toDoList.pop(new Task("kanapki2","zrobić kanapki33"));
+toDoList.pop(new Task("kanapki3","zrobić kanapki33"));
+
+var nextRow = document.createElement("row");
+
+$("button.add").click(function(){
+    new Task(#exampleInputEmail3,#exampleInputPassword3);
+});
