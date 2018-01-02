879411f70b30a8fa896e5eaee8eaa25b1e4ce212
aleksandrajagodzinska <aleksandra.berry01@gmail.com>
2018-01-02 11:52:07 +0100

    button added

--- ToDoAplication.js
+++ ToDoAplication.js
@@ -10,7 +10,6 @@ for (i = 0; i < myNodelist.length; i++) {
     myNodelist[i].appendChild(span);
 }
 
-
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -19,15 +18,6 @@ for (i = 0; i < close.length; i++) {
         div.style.display = "none";
     }
 }
-
-
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
-
 
 function newElement() {
     var li = document.createElement("li");
